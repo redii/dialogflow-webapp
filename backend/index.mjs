9fe9efa6fb19237c19aea82305c25279e85f6e7d
akmann <akmann.henry@gmail.com>
2022-05-11 11:51:38 +0200

    minor changes

--- backend/index.mjs
+++ backend/index.mjs
@@ -7,7 +7,10 @@ const df_url =
 
 const server = http.createServer(async (req, res) => {
     try {
-        res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080")
+        res.setHeader(
+            "Access-Control-Allow-Origin",
+            "http://localhost:8080, https://redii.github.io/dialogflow-webapp"
+        )
         res.setHeader("Access-Control-Allow-Credentials", true)
         res.setHeader("Content-Type", "application/json")
 
