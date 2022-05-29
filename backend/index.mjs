d58ebbab8d7630d58e84665b719713d527131ee4
akmann <akmann.henry@gmail.com>
2022-05-29 13:48:32 +0200

    added default and inclusive agents

--- backend/index.mjs
+++ backend/index.mjs
@@ -2,8 +2,12 @@ import http from "http"
 import url from "url"
 import got from "got"
 
-const df_url =
-    "https://console.dialogflow.com/v1/integrations/messenger/webhook/4e508c7e-69cb-43bf-b32a-44d54f6b3431/sessions/webdemo-25036c1f-dd66-4d05-d948-45bddbcda347?platform=webdemo"
+// prettier-ignore
+const df_urls = {
+    dev: "https://console.dialogflow.com/v1/integrations/messenger/webhook/4e508c7e-69cb-43bf-b32a-44d54f6b3431/sessions/webdemo-25036c1f-dd66-4d05-d948-45bddbcda347?platform=webdemo",
+    default: "https://console.dialogflow.com/v1/integrations/messenger/webhook/18dc7865-baf4-4d1b-9dc4-2980d30bf725/sessions/webdemo-439eaa42-c853-07d8-b10c-2e5edb39eb41?platform=webdemo",
+    inclusive: "https://console.dialogflow.com/v1/integrations/messenger/webhook/15ac00e6-6b68-44a9-8a2c-5c9bec6cae6c/sessions/webdemo-a872f2f4-e571-ebef-fdce-b1a261dec04a?platform=webdemo"
+}
 
 const server = http.createServer(async (req, res) => {
     try {
@@ -11,9 +15,9 @@ const server = http.createServer(async (req, res) => {
         // res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080")
         res.setHeader("Access-Control-Allow-Credentials", true)
         res.setHeader("Content-Type", "application/json")
-
         var params = url.parse(req.url, true).query
 
+        const url = df_urls[params.bot]
         const query = {
             queryInput: {
                 text: {
@@ -23,10 +27,7 @@ const server = http.createServer(async (req, res) => {
             },
         }
 
-        const temp = await got.post(df_url, {
-            json: query,
-        })
-
+        const temp = await got.post(url, { json: query })
         const result = JSON.parse(temp.request.response.body.substring(5))
 
         res.end(JSON.stringify(result))
--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,10 +1,38 @@
 <script>
     import { Row, Col } from "sveltestrap"
-    import Chat from "./Chat.svelte"
+    import ChatBot from "./ChatBot.svelte"
 </script>
 
+<!-- prettier-ignore -->
 <main>
-    <Chat />
+    <Row>
+        <Col md={6}>
+            <ChatBot
+                botType="default"
+                botName="Philipp Meier"
+                botStatus="Dekan der Fakultät Maschinenbau"
+                fontFamily="Times New Roman"
+                fontSize="14px"
+                headerBackgroundColor="lightgrey"
+                chatBackgroundColor="lightgrey"
+                fontColorBot="lightgrey"
+                fontColorUser="lightgrey"
+                bubbleColorBot="black"
+                bubbleColorUser="#363b3f" />
+        </Col>
+        <Col md={6}>
+            <ChatBot
+                botType="inclusive"
+                botName="Anushka"
+                botImage="images/anushka.png"
+                fontFamily="Arial"
+                fontSize="20px"
+                fontColorBot="black"
+                fontColorUser="black"
+                bubbleColorBot="#bccee1"
+                bubbleColorUser="#b0efb0" />
+        </Col>
+    </Row>
 </main>
 
 <style>
