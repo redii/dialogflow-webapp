4ade17f2a2fa32a456aa3ebee7f244eefffb63b2
akmann <akmann.henry@gmail.com>
2022-06-20 18:31:14 +0200

    minor changes

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,14 +1,51 @@
 <script>
+    import queryString from "query-string"
+
     import { Row, Col } from "sveltestrap"
+    import ChatBot from "./ChatBot.svelte"
+
+    const bot = queryString.parse(location.search).bot
 </script>
 
 <main>
     <Row>
         <Col md={{ size: 6, offset: 3 }}>
-            <h1>Chatbots</h1>
-            <a href="/dialogflow-webapp/chatbot-1.html">Default ChatBot</a>
-            <br />
-            <a href="/dialogflow-webapp/chatbot-2.html">Inclusive ChatBot</a>
+            {#if bot === "1"}
+                <ChatBot
+                    botType="default"
+                    botName="Dr. Franz Zweistein"
+                    botImage="https://i.ibb.co/k4NF2wv/max.jpg"
+                    startMessage="Hallo 👋🏻"
+                    typingWithName={false}
+                    fontFamily="Times New Roman"
+                    fontSize="14px"
+                    headerBackgroundColor="lightgrey"
+                    chatBackgroundColor="lightgrey"
+                    fontColorBot="lightgrey"
+                    fontColorUser="lightgrey"
+                    bubbleColorBot="black"
+                    bubbleColorUser="#363b3f"
+                />
+            {:else if bot === "2"}
+                <ChatBot
+                    botType="inclusive"
+                    botName="Aylin"
+                    botImage="https://i.ibb.co/5L02hP2/aylin.jpg"
+                    fontFamily="Arial"
+                    fontSize="20px"
+                    fontColorBot="black"
+                    fontColorUser="black"
+                    bubbleColorBot="#bccee1"
+                    bubbleColorUser="#b0efb0"
+                />
+            {:else}
+                <h1>Chatbots</h1>
+                <a href="/dialogflow-webapp/?bot=default">Default ChatBot</a>
+                <br />
+                <a href="/dialogflow-webapp/?bot=inclusive">
+                    Inclusive ChatBot
+                </a>
+            {/if}
         </Col>
     </Row>
 </main>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,5 @@
 import App from "./App.svelte"
-import DefaultBot from "./DefaultBot.svelte"
-import InclusiveBot from "./InclusiveBot.svelte"
 
 export const app = new App({
-    target: document.getElementById("home"),
-})
-export const bot1 = new DefaultBot({
-    target: document.getElementById("default-bot"),
-})
-export const bot2 = new InclusiveBot({
-    target: document.getElementById("inclusive-bot"),
+    target: document.body,
 })
